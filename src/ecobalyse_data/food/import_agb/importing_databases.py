663fdf41ba647ad66afe3e7f76f3115d02361b0d
paulboosz <paulboosz@gmail.com>
2022-11-28 14:14:17 +0100

    update gitignore

--- src/ecobalyse_data/food/import_agb/importing_databases.py
+++ src/ecobalyse_data/food/import_agb/importing_databases.py
@@ -40,7 +40,7 @@ def main():
     if "agribalyse3" in bw.databases:
         print("Database has already been imported.")
     else:
-        agb_csv_zip_filepath = r"../agribalyse3_no_param.CSV.zip"
+        agb_csv_zip_filepath = r"agribalyse3_no_param.CSV.zip"
         agb_csv_filepath = r"agribalyse3_no_param.CSV"
 
         with ZipFile(agb_csv_zip_filepath) as zf:
