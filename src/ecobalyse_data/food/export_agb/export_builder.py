2a665be748a61abe2436b9c1f7168cb09a2dfb78
Mathieu Agopian <mathieu@agopian.info>
2022-12-01 15:36:04 +0100

    export_builder.py can now export complex ingredients

--- src/ecobalyse_data/food/export_agb/export_builder.py
+++ src/ecobalyse_data/food/export_agb/export_builder.py
@@ -4,12 +4,14 @@
 """Export de l'impact d'une liste de processes
 exemple : python export_builder.py ../../../../../ecobalyse/public/data/impacts.json builder_processes_to_export.txt"""
 
+import copy
 import json
 import argparse
 import brightway2 as bw
 from collections import defaultdict
 from ecobalyse_data.food.impacts import impacts
 import pandas as pd
+import uuid
 import ecobalyse_data
 
 processes_kind = {
@@ -154,26 +156,90 @@ def compute_pef(impacts_ecobalyse, impacts_dic):
     return pef
 
 
-def compute_lca(processes, lcas):
-    with open(args.impacts_file, "r") as f:
-        impacts_ecobalyse = json.load(f)
+def impacts_for_activity(activity, lcas, impacts_ecobalyse):
+    activity_impacts = {}
+    for impact in impacts.keys():
+        lca = lcas[impact]
 
+        demand = {activity: 1}
+        lca.redo_lcia(demand)
+        activity_impacts[impact] = lca.score
+    activity_impacts["pef"] = compute_pef(impacts_ecobalyse, activity_impacts)
+    return activity_impacts
+
+
+def compute_lca(processes, lcas, impacts_ecobalyse):
     num_processes = len(processes)
     print(f"Calcul de l'impact pour {num_processes} procédés")
-    for index, (activity, value) in enumerate(processes.items()):
-        for impact in impacts.keys():
-            lca = lcas[impact]
-
-            demand = {activity: 1}
-            lca.redo_lcia(demand)
-            processes[activity]["impacts"][impact] = lca.score
-
-        processes[activity]["impacts"]["pef"] = compute_pef(
-            impacts_ecobalyse, processes[activity]["impacts"]
-        )
+    for index, activity in enumerate(processes):
+        impacts = impacts_for_activity(activity, lcas, impacts_ecobalyse)
+        processes[activity]["impacts"] = impacts
         if index % 10 == 0:
             print(f"{round(index * 100 / num_processes)}%", end="\r")
     print("100%")
+
+
+class ProcessNotFoundError(Exception):
+    def __init__(self, process_id):
+        self.message = f"Procédé non trouvé pour l'id {process_id}"
+        super().__init__(self.message)
+
+
+def get_process_by_id(processes, process_id):
+    for process in processes.values():
+        if process["simapro_id"] == process_id:
+            return process
+    raise ProcessNotFoundError(process_id)
+
+
+def compute_ingredient_list(agribalyse_db, processes, lcas, impacts_ecobalyse):
+    with open("ingredients_base.json", "r") as f:
+        ingredients_base = json.load(f)
+
+    new_processes = []
+
+    for ingredient in ingredients_base:
+        for variant_name in ingredient["variants"]:
+            variant = ingredient["variants"][variant_name]
+            if isinstance(variant, dict):
+                # This is a complex ingredient, we need to create a new process from the elements we have.
+                complex_ingredient_default = get_process_by_id(
+                    processes, ingredient["default"]
+                )
+                ratio = variant["ratio"]
+
+                simple_ingredient_default = agribalyse_db.search(
+                    variant["simple_ingredient_default"]
+                )[0]
+                simple_ingredient_default_impacts = impacts_for_activity(
+                    simple_ingredient_default, lcas, impacts_ecobalyse
+                )
+                simple_ingredient_variant = agribalyse_db.search(
+                    variant["simple_ingredient_variant"]
+                )[0]
+                simple_ingredient_variant_impacts = impacts_for_activity(
+                    simple_ingredient_variant, lcas, impacts_ecobalyse
+                )
+
+                new_process = copy.deepcopy(complex_ingredient_default)
+                new_process[
+                    "name"
+                ] = f"{ingredient['id']}, {variant_name}, constructed by ecobalyse"
+                new_process["system_description"] = "ecobalyse"
+                new_process["simapro_id"] = str(uuid.uuid4())
+                for impact in new_process["impacts"]:
+                    # Formula: Impact farine bio = impact farine conventionnel + 1.16 *( impact blé bio -  impact blé conventionnel)
+                    new_process["impacts"][impact] = new_process["impacts"][
+                        impact
+                    ] + ratio * (
+                        simple_ingredient_variant_impacts[impact]
+                        - simple_ingredient_default_impacts[impact]
+                    )
+                ingredient["variants"][variant_name] = new_process["simapro_id"]
+
+                new_processes.append(new_process)
+
+    return (ingredients_base, new_processes)
 
 
 def export_json(content, filename):
@@ -216,10 +282,26 @@ if __name__ == "__main__":
     random_process = next(iter(processes))
     lcas = init_lcas({random_process: 1})
 
-    compute_lca(processes, lcas)
+    with open(args.impacts_file, "r") as f:
+        impacts_ecobalyse = json.load(f)
+
+    compute_lca(processes, lcas, impacts_ecobalyse)
+
+    # Export the ingredients.json file
+    ingredients_export_file = "ingredients.json"
+    (ingredient_list, new_processes) = compute_ingredient_list(
+        agb, processes, lcas, impacts_ecobalyse
+    )
+    print(
+        f"Export de {len(ingredient_list)} ingrédients vers {ingredients_export_file}"
+    )
+    export_json(ingredient_list, ingredients_export_file)
 
     # reformat processes in a list of dictionaries
     processes_list = list(processes.values())
+
+    # Add the new processes we computed for the complex ingredients
+    processes_list += new_processes
 
     processes_export_file = "builder_processes.json"
     print(f"Export de {len(processes_list)} procédés vers {processes_export_file}")
