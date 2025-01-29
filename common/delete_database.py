e3387473c3316377dcd23d8711d8cfd93f8dbdf2
Vincent Jousse <vincent@jousse.org>
2025-01-29 22:52:33 +0100

    chore: use project from settings

--- common/delete_database.py
+++ common/delete_database.py
@@ -2,12 +2,15 @@
 import sys
 
 import bw2data
+from config import settings
 
 if len(sys.argv) != 2:
     print("Provide the database as 1st arg")
     sys.exit(1)
 database = sys.argv[1]
-bw2data.projects.set_current("ecobalyse")
+
+bw2data.projects.set_current(settings.bw.project)
+
 print(f"Deleting database: {database}...")
 if database in bw2data.databases:
     del bw2data.databases[database]
