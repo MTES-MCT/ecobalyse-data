21fb79d71089723ae3849b5fd7a9e099d41c8da7
Vincent Jousse <vincent@jousse.org>
2025-02-26 18:36:20 +0100

    fix: use python extract function

--- ecobalyse_data/tests.py
+++ ecobalyse_data/tests.py
@@ -1,21 +1,8 @@
 import shutil
-import tempfile
-from pathlib import Path
 
 from bw2data.project import projects
-from bw2io.backup import _extract_single_directory_tarball
 
 
 def restore_archived_project(archive_path):
-    with tempfile.TemporaryDirectory() as td:
-        extracted_path = _extract_single_directory_tarball(
-            filepath=Path(archive_path),
-            output_dir=td,
-        )
-
-        base_data_dir, _ = projects._get_base_directories()
-        print(
-            f"-> Restore archived project from {archive_path}, {extracted_path} to {base_data_dir}"
-        )
-
-        shutil.copytree(extracted_path, base_data_dir, dirs_exist_ok=True)
+    base_data_dir, _ = projects._get_base_directories()
+    shutil.unpack_archive(archive_path, base_data_dir)
