7e2333f90d95cc9c9a6de41ed49cc3784655805f
Vincent Jousse <vincent@jousse.org>
2025-02-26 17:51:57 +0100

    chore: remove dead code

--- ecobalyse_data/tests.py
+++ ecobalyse_data/tests.py
@@ -1,35 +1,9 @@
-import atexit
-import os
 import shutil
 import tempfile
 from pathlib import Path
 
-import bw2data
-import wrapt
-from bw2data import config
 from bw2data.project import projects
 from bw2io.backup import _extract_single_directory_tarball
-
-from config import settings
-
-
-@wrapt.decorator
-def bw2test(wrapped, instance, args, kwargs):
-    config.dont_warn = True
-    config.is_test = True
-    tempdir = Path(tempfile.mkdtemp())
-    os.environ["BRIGHTWAY2_DIR"] = str(tempdir)
-
-    projects.change_base_directories(
-        base_dir=tempdir,
-        base_logs_dir=tempdir,
-        project_name=settings.bw.project,
-        update=False,
-    )
-    projects._is_temp_dir = True
-    atexit.register(shutil.rmtree, tempdir)
-    bw2data.projects.set_current(settings.bw.project)
-    return wrapped(*args, **kwargs)
 
 
 def restore_archived_project(archive_path):
