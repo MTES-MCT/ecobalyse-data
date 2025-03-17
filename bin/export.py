ac19da71af80577fd719d31d7b33cbd0c7c9abf8
Vincent Jousse <vincent@jousse.org>
2025-03-17 16:21:59 +0100

    refactor: one command to rule them all

--- bin/export.py
+++ bin/export.py
@@ -3,9 +3,12 @@
 
 import logging
 import os
+from enum import Enum
+from typing import List
 
 import typer
 from bw2data.project import projects
+from typing_extensions import Annotated
 
 from config import get_absolute_path, settings
 from ecobalyse_data import export
@@ -14,85 +17,49 @@ from ecobalyse_data.logging import logger
 app = typer.Typer()
 
 
-@app.command()
-def food():
-    """
-    Export food
-    """
-
-    pass
+class Domain(str, Enum):
+    food = "food"
+    object = "object"
+    textile = "textile"
 
 
 @app.command()
-def textile(
+def processes(
+    domain: Annotated[
+        List[Domain],
+        typer.Option(
+            help="The domain you want to export processes for. By default, export everything."
+        ),
+    ] = [d.value for d in Domain],
     plot: bool = typer.Option(False, "--plot", "-p"),
     verbose: bool = typer.Option(False, "--verbose", "-v"),
 ):
     """
-    Export textile
+    Export processes
     """
 
     if verbose:
         logger.setLevel(logging.DEBUG)
-
-    logger.info("INFO")
-    logger.debug("DEBUG")
 
     dirs_to_export_to = [settings.output_dir]
 
     if settings.local_export:
         dirs_to_export_to.append(os.path.join(get_absolute_path("."), "public", "data"))
 
-    export.run(
-        activities_path=os.path.join(
-            get_absolute_path(settings.textile.dirname), "activities.json"
-        ),
-        aggregated_relative_file_path=os.path.join(
-            settings.textile.dirname, settings.processes_aggregated_file
-        ),
-        impacts_relative_file_path=os.path.join(
-            settings.textile.dirname, settings.processes_impacts_file
-        ),
-        dirs_to_export_to=dirs_to_export_to,
-        verbose=verbose,
-        plot=plot,
-    )
-
-
-@app.command()
-def object(
-    plot: bool = typer.Option(False, "--plot", "-p"),
-    verbose: bool = typer.Option(False, "--verbose", "-v"),
-):
-    """
-    Export object
-    """
-
-    if verbose:
-        logger.setLevel(logging.DEBUG)
-
-    logger.info("INFO")
-    logger.debug("DEBUG")
-
-    dirs_to_export_to = [settings.output_dir]
-
-    if settings.local_export:
-        dirs_to_export_to.append(os.path.join(get_absolute_path("."), "public", "data"))
-
-    export.run(
-        activities_path=os.path.join(
-            get_absolute_path(settings.object.dirname), "activities.json"
-        ),
-        aggregated_relative_file_path=os.path.join(
-            settings.object.dirname, settings.processes_aggregated_file
-        ),
-        impacts_relative_file_path=os.path.join(
-            settings.object.dirname, settings.processes_impacts_file
-        ),
-        dirs_to_export_to=dirs_to_export_to,
-        verbose=verbose,
-        plot=plot,
-    )
+    for d in domain:
+        dirname = settings.get(d.value).dirname
+        export.run(
+            activities_path=os.path.join(get_absolute_path(dirname), "activities.json"),
+            aggregated_relative_file_path=os.path.join(
+                dirname, settings.processes_aggregated_file
+            ),
+            impacts_relative_file_path=os.path.join(
+                dirname, settings.processes_impacts_file
+            ),
+            dirs_to_export_to=dirs_to_export_to,
+            verbose=verbose,
+            plot=plot,
+        )
 
 
 if __name__ == "__main__":
