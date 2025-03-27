470ed6384fb58f256e81bb84dad66e402e0367d2
Vincent Jousse <vincent@jousse.org>
2025-03-27 16:52:09 +0100

    chore: remove debug

--- backend/src/app/domain/processes/services.py
+++ backend/src/app/domain/processes/services.py
@@ -1,18 +1,12 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from advanced_alchemy.repository import (
     SQLAlchemyAsyncRepository,
 )
 from advanced_alchemy.service import (
     SQLAlchemyAsyncRepositoryService,
-    schema_dump,
 )
 from app.db import models as m
-
-if TYPE_CHECKING:
-    from advanced_alchemy.service import ModelDictT
 
 __all__ = ("ProcessService",)
 
@@ -28,10 +22,3 @@ class ProcessService(SQLAlchemyAsyncRepositoryService[m.ProcessModel]):
     repository_type = ProcessRepository
 
     match_fields = ["name"]
-
-    async def to_model_on_upsert(
-        self, data: ModelDictT[m.ProcessModel]
-    ) -> ModelDictT[m.ProcessModel]:
-        data = schema_dump(data)
-        print(f"###### -> to model on upsert {data}")
-        return data
