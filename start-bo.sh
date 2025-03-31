1de507323a9263e7be3449c7005685946d5b2f57
Vincent Jousse <vincent@jousse.org>
2025-03-31 14:52:57 +0200

    chore: run nginx

--- start-bo.sh
+++ start-bo.sh
@@ -1,7 +1,10 @@
 cd backend
 uv run backend database upgrade --no-prompt
-uv run backend run
+uv run backend run &
+cd ..
 
+# nginx
+bin/run &
 
 # if the current shell is killed, also terminate all its children
 trap "pkill SIGTERM -P $$" SIGTERM
