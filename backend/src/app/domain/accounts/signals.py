8f3f87be7581cf5bb1e2304efa9ff1c7f9a8b350
Vincent Jousse <vincent@jousse.org>
2025-04-17 16:41:06 +0200

    chore: prepare sending emails

--- backend/src/app/domain/accounts/signals.py
+++ backend/src/app/domain/accounts/signals.py
@@ -1,7 +1,13 @@
 from __future__ import annotations
 
+import urllib.parse
+
+import emails
 import structlog
+from emails.template import JinjaTemplate as T
 from litestar.events import listener
+
+from app.config import get_settings
 
 logger = structlog.get_logger()
 
@@ -16,3 +22,28 @@ async def send_magic_link_email_event_handler(
         email: The email we should send the magic link to
     """
     await logger.adebug(f"Sending magic link email to {email}")
+    settings = get_settings()
+
+    message = emails.html(
+        subject=T("Connexion à Ecobalyse"),
+        html=T(
+            '<p>Magic link <a href="http://test.com/api/access/login?username={{ email }}&token={{ token }}">login</a></p>'
+        ),
+        text=T(
+            "Magic link: http://test.com/api/access/login?username={{ email }}&token={{ token }}"
+        ),
+        mail_from=("Ecobalyse", "ecobalyse@test.com"),
+    )
+
+    message.send(
+        to=("Test user", email),
+        render={
+            "email": urllib.parse.quote_plus(email),
+            "token": urllib.parse.quote_plus("testtoken"),
+        },
+    )
+
+    if settings.email.SERVER_HOST is None:
+        await logger.adebug("No email SERVER_HOST configured don’t send the email.")
+        print(message.html_body)
+        print(message.text_body)
