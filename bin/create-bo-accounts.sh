5f30548cb806bee812333542197c2e3405b4e48f
Vincent Jousse <vincent@jousse.org>
2025-04-29 20:41:06 +0200

    chore: reset review app db on each deploy

--- bin/create-bo-accounts.sh
+++ bin/create-bo-accounts.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )
+cd $ROOT_DIR
 
 if [ "$IS_REVIEW_APP" == "true" ]; then
    echo "-> In review app";
+   cd backend
+   uv run backend database drop-all --no-prompt
+   uv run backend database upgrade --no-prompt
+   cd ..
 fi
