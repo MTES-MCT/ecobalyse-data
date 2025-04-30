9231124ea36e774b4c0645f5f929fc17cd00b44b
Vincent Jousse <vincent@jousse.org>
2025-04-30 16:45:07 +0200

    chore: create backend admins

--- bin/create-bo-accounts.sh
+++ bin/create-bo-accounts.sh
@@ -4,11 +4,16 @@ ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )
 cd $ROOT_DIR
 
 if [ "$IS_REVIEW_APP" == "true" ]; then
-   echo "-> In review app";
+   echo "-> In review app, resetting DB";
    cd backend
    uv run backend database drop-all --no-prompt
    uv run backend database upgrade --no-prompt
-   uv run backend users create-user --email vincent.jousse@beta.gouv.fr --name "Vinc Beta" --superuser
-   uv run backend users create-user --email vincent@jousse.org --name "Vince Perso" --superuser
+   # Test if variable is set
+   if test -n "${BACKEND_ADMINS:+x}"; then
+     IFS=',' read -ra ADDR <<< "$BACKEND_ADMINS"
+     for email in "${ADDR[@]}"; do
+       uv run backend users create-user --email $email --superuser
+     done
+   fi
    cd ..
 fi
