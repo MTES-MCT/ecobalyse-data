c00d294d2ceec5f26368c7d879559be7efdd8626
Christophe Combelles <ccomb@free.fr>
2025-06-03 20:55:49 +0200

    updated the simapro projects

--- common/__init__.py
+++ common/__init__.py
@@ -52,7 +52,7 @@ def spproject(activity):
             # return a non existing project to force looking at brightway
             return ("EcobalyseIsNotASimaProProject", "")
         case "Ecoinvent 3.9.1":
-            return ("ADEME UPR", "")
+            return ("Ecoinvent 3.9.1", "Ecoinvent 3.9.1 - unit")
         case "Woolmark":
             return ("Woolmark", "")
         case "PastoEco":
@@ -62,7 +62,7 @@ def spproject(activity):
         case "Agribalyse 3.1.1":
             return ("Agribalyse 3.1.1", "")
         case "Agribalyse 3.2":
-            return ("Agribalyse 3.2", "AGRIBALYSE - unit")
+            return ("Agribalyse 3.2", "Agribalyse 3.2 - unit")
         case _:
             raise Exception("Unknown database")
 
