13a131be088bc96a861302253bb865423be989d9
Christophe Combelles <ccomb@free.fr>
2025-06-30 12:54:13 +0200

    compare processes by name

--- bin/lcia_info.py
+++ bin/lcia_info.py
@@ -135,6 +135,10 @@ def compare_processes(
             help="The trigram name ('ecs', 'etf', …) of the impact you want to compare. You can specify multiple `--impact`. If not specified, all impacts are compared.",
         ),
     ] = [],
+    group_by_name: Annotated[
+        bool,
+        typer.Option(help="Group comparison by process name."),
+    ] = False,
 ):
     """
     Compare two `processes_impacts.json` files
@@ -142,12 +146,35 @@ def compare_processes(
 
     first_processes = json.load(first_file)
     second_processes = json.load(second_file)
+
     display_changes(
         "id",
         first_processes,
         second_processes,
         only_impacts=impact,
     )
+
+    if group_by_name:
+        second_processes_by_name = {p["displayName"]: p for p in second_processes}
+        for process in first_processes:
+            if process["displayName"] in second_processes_by_name:
+                display_changes(
+                    "id",
+                    [process],
+                    [second_processes_by_name[process["displayName"]]],
+                    only_impacts=impact,
+                )
+            else:
+                logger.warning(
+                    f"Can’t find process with displayName '{process['displayName']}' in second file, skipping compaison."
+                )
+    else:
+        display_changes(
+            "id",
+            first_processes,
+            second_processes,
+            only_impacts=impact,
+        )
 
 
 @app.command()
