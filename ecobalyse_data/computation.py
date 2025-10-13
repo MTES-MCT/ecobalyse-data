ca5e595fca03f6dd785d66d90e53dc77a2fdef10
paulboosz <paulboosz@gmail.com>
2025-10-13 17:25:46 +0200

    chore: clarify name function and comment

--- ecobalyse_data/computation.py
+++ ecobalyse_data/computation.py
@@ -287,10 +287,8 @@ def activity_to_process_with_impacts(
 
     bw_activity["unit"] = unit
 
-    # Some hardcoded activities don't have a name
-    name = bw_activity.get(
-        "name", eco_activity.get("displayName", eco_activity.get("name"))
-    )
+    # Some hardcoded activities don't have a name, in that case take the ecobalyse displayName
+    name = bw_activity.get("name", eco_activity["displayName"])
 
     # If we don't have a real bw_activity instance but a dict instead, don't try to get
     # comments from the database
